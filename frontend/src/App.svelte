3fd656c1928e455e1f581bf45582db89306b8d55
Robert Valek <rvalek@intersog.com>
2021-01-27 19:20:44 +0200

    chart inside a svelte component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
+  import Chart from "./Chart.svelte";
 </script>
 
 <main>
-	<h1>Hello!</h1>
+  <h1>Hello!</h1>
+  <Chart />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
