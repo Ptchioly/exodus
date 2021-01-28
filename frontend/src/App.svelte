e699708ade0a6ba1b247b103d89dc76c8b6b4016
Robert Valek <rvalek@intersog.com>
2021-01-28 19:10:30 +0200

    first tail

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
   import TailwindCss from "./TailwindCss.svelte";
+  import TailChart from "./charts/TailChart.svelte";
 </script>
 
 <TailwindCss />
 <main>
   <h1 class="text-gray-500 font-extralight text-5xl">Hello,</h1>
   <h1 class="font-black italic tracking-wide text-indigo-600 text-5xl">
-    SOBAKA
+    SOBAKA,
   </h1>
   <h1 class="font-black italic tracking-wide text-5xl">SMOTRI:</h1>
+  <br />
+  <TailChart />
 </main>
 
 <style global lang="postcss">
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
   }
 
@@ -24,11 +26,5 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
---
+++ frontend/src/charts/TailChart.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  const data = [
+    {
+      name: "Taxi",
+      currMonth: 560,
+      prevMonth: 815,
+      limit: 760,
+    },
+    {
+      name: "Groceries",
+      currMonth: 910,
+      prevMonth: 1300,
+      limit: 1500,
+    },
+  ];
+</script>
+
+<div class="m-auto w-3/5">
+  {#each data as { name, currMonth, prevMonth, limit }, i}
+    <div class="flex flex-col md:flex-row justify-between">
+      <span class="inline-flex w-1/12">{name}</span>
+      <div class="inline-flex w-5/6">
+        <div class="inline-flex ">
+          <span>{currMonth}</span>
+        </div>
+        <div class="inline-flex">
+          <span>{prevMonth}</span>
+        </div>
+        <div class="inline-flex">
+          <span>{limit}</span>
+        </div>
+      </div>
+      <div class="inline-flex w-1/12">
+        <span>{limit}</span>
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style local lang="postcss">
+</style>
