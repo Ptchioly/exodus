32cacc4c088b023a54e5fb3d0c3e72c7b92fbb2b
Andrey Okun <aokun@intersog.com>
2021-01-29 20:37:57 +0200

    Rearranged files, added comments

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import TailwindCss from './TailwindCss.svelte';
-  import SignIn from './signInPage/SignIn.svelte';
-  import SignUp from './signInPage/SignUp.svelte';
+  import SignIn from './routes/SignIn.svelte';
+  import SignUp from './routes/SignUp.svelte';
   import { Router, Route } from 'svelte-routing';
 
   export let url = '';
 </script>
 
 <TailwindCss />
-<main class="font-main">
+<main class="font-main m-auto text-center">
   <Router {url}>
     <Route path="signIn" component={SignIn} />
     <Route path="signUp" component={SignUp} />
@@ -16,40 +16,28 @@
   </Router>
 </main>
 
+<svelte:head>
+  <link rel="icon" type="image/png" href="images/favicon.png" />
+</svelte:head>
+
+<!-- Probably should be placed in html tag with tailwind and using 'rem' instead of 'px' -->
 <style global lang="postcss">
-  main {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-
-  /* Small devices (portrait tablets and large phones, 600px and up) */
   @media only screen and (min-width: 600px) {
     html {
       font-size: 12px;
     }
   }
-
-  /* Medium devices (landscape tablets, 768px and up) */
   @media only screen and (min-width: 768px) {
     html {
       font-size: 18px;
     }
   }
-
-  /* Large devices (laptops/desktops, 992px and up) */
   @media only screen and (min-width: 992px) {
     html {
       font-size: 22px;
     }
   }
 
-  /* Extra large devices (large laptops and desktops, 1200px and up) */
   @media only screen and (min-width: 1200px) {
     html {
       font-size: 24px;
