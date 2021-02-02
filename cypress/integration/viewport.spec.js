5e50164ebdfdfde794ff08b573b9170958c2421a
EKHOLODILIN <yevgeni.kh@gmail.com>
2021-02-02 14:52:52 +0200

    Tests: updated viewport spec

---
+++ cypress/integration/viewport.spec.js
@@ -0,0 +1,15 @@
+const sizes = ['iphone-6', 'ipad-2', [1024, 768]] // test viewports, update with relevant ones
+describe(`resolution tests`, () => {
+    sizes.forEach((size) => {
+      it(`displays page properly`, () => {
+          if (Cypress._.isArray(size)) {
+              cy.viewport(size[0], size[1])
+          } else {
+              cy.viewport(size)
+          }
+          cy.visit(`/exodus/`)
+          // asserts go here
+          //also maybe add different orientations
+        })
+    })
+})
