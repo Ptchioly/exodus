4125804bc830e41c201b7e235433af92150e76e4
Lev <lbrekhov@intersog.com>
2021-02-03 13:37:30 +0200

    added personal info endpoint

--- backend/src/routes/monobank/statement.ts
+++ backend/src/routes/monobank/statement.ts
@@ -2,30 +2,42 @@ import { Router } from 'express';
 import fetch from 'node-fetch';
 import { configs } from '../../config';
 import { getItem } from '../../dynamoAPI';
+import { endpointRespond } from '../../utils';
 import { authenticateToken } from '../auth/validate';
 import { isFailure } from '../types/guards';
-import { endpoint } from './endpoints';
+import { StatementRequest } from '../types/types';
+import { requests } from './endpoints';
 
 export const statement = Router();
+
+const checkStatementParams = ({
+  account,
+  from,
+  to,
+}: Partial<StatementRequest>): StatementRequest => {
+  return {
+    account: account || 0,
+    from: from || Date.now() - 2678400000,
+    to,
+  };
+};
 
 statement.get(
   '/statement/:account/:from/:to',
   authenticateToken,
   async (req: any, res) => {
-    const { account, from, to } = req.params; // check exist
+    const { account, from, to } = checkStatementParams(req.params); // check exist
     const userFromDB = await getItem(configs.USER_TABLE, {
       username: req.user.data,
     });
+    const respond = endpointRespond(res);
     if (!isFailure(userFromDB)) {
-      const data = await fetch(
-        endpoint(`personal/statement/${account}/${from}/${to}`),
-        {
-          headers: {
-            'X-Token': userFromDB.Item.xtoken,
-          },
-        }
-      );
-      res.send(await data.json());
-    } else res.status(400).json('param pam pam').end();
+      const data = await fetch(requests.statement(account, from, to), {
+        headers: {
+          'X-Token': userFromDB.Item.xtoken,
+        },
+      }).then((el) => el.json());
+      return respond.SuccessResponse(data);
+    } else return respond.FailureResponse('Failed to get statement');
   }
 );
