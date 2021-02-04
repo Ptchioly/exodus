0375556b8db5f7b8aa04783bd18dbe70eaaec53c
Elena Chornobai <echornobai@intersog.com>
2021-02-04 15:11:08 +0200

    Added more stubs in login and signup test suites; added API command stubs for login and signup

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,12 +8,68 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
+const apiUrl = Cypress.env('apiUrl')
+
 //
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
+// -- API login command --
+Cypress.Commands.add("login", (phone, password) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://ptchioly.github.io/login',
+        body: {
+            user: {
+                phone: phone,
+                password: password
+            }
+        }
+    }).then((resp) => {
+        cy.setCookie('jwt', resp.body.item.token, { expiry: 86400000 })
+
+    })
+})
+
+// -- a custom command to login using XHR call and set the received cookie --
+// log in with default user 
+Cypress.Commands.add('login', (user = Cypress.env('user')) => {
+    cy.getLoginToken(user).then(token => {
+        cy.setCookie('jwt', token, { expiry: 86400000 })
+    })
+    cy.visit('/exodus')
+    cy.get('[data-automation-id=user-profile]').should('be.visible')
+})
+
+
+// -- custom command to return a token after logging in --
+Cypress.Commands.add('getLoginToken', (user = Cypress.env('user')) => {
+    return cy
+      .request('POST', `${apiUrl}/api/users/login`, {
+        user: Cypress._.pick(user, ['phone', 'password'])
+      })
+      .its('body.item.token')
+      .should('exist')
+  })
+
+  // creates a user with email and password
+// defined in cypress.json environment variables
+// if the user already exists, ignores the error
+// or given user info parameters
+Cypress.Commands.add('registerUserIfNeeded', (options = {}) => {
+    const defaults = {
+      // phone, password
+      ...Cypress.env('user')
+    }
+    const user = Cypress._.defaults({}, options, defaults)
+    cy.request({
+      method: 'POST',
+      url: `${apiUrl}/api/users`,
+      body: {
+        user
+      },
+      failOnStatusCode: false
+    })
+  })
+
+  // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
