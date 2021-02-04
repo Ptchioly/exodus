cdbcd188de4b1b42995432a9ea5c3fcfe24a1814
Elena Chornobai <echornobai@intersog.com>
2021-02-04 14:02:28 +0200

    Added more stubs in login test suite

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,9 +7,36 @@ describe.only('Login', () => {
     cy.visit('/exodus/')
   })
 
-  // it('does not work with wrong credentials', () => {
+  // it('greets with Sign in', () => {
+  //   cy.contains('h1', 'Sign In')
   // })
 
-  it('logs in', () => { cy.window().should('have.property', 'top') })
+  // it('displays register page on "Join now" click', () => {
+  //   cy.contains('Join now').click()
+  //     .should('have.attr', 'href', '/signup')
+  // })
+
+  // it('requires phone number', () => {
+  //   cy.get('form').contains('Sign in').click()
+  // })
+
+  // it('requires password', () => {
+  //   cy.get('[data-automation-id=phone]').type('1234567890{enter}')
+  //   cy.get('.error-msg')
+  //     .should('contain', 'password number can\'t be blank')
+  // })
+
+  // it('does not work with wrong credentials', () => {
+  //   cy.get('[data-automation-id=phone]').type('1234567890{enter}')
+  //   cy.get('[data-automation-id=password]').type('invalid{enter}')
+  //   cy.get('.error-msg')
+  //     .should('contain', 'phone or password is invalid')
+  // })
+
+  it('dipslays home page on successful login', () => { 
+    // cy.get('[data-automation-id=phone]').type('1234567890{enter}')
+    // cy.get('[data-automation-id=password]').type('valid{enter}')
+    cy.window().should('have.property', 'top')
+   })
 
 })
