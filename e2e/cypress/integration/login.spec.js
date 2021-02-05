8600ff1cd447cc4327f3a524d4f62cb727b134b5
Elena Chornobai <echornobai@intersog.com>
2021-02-05 10:10:14 +0200

    Moved cypress and its configs to a separate dir

--- e2e/cypress/integration/login.spec.js
+++ e2e/cypress/integration/login.spec.js
@@ -21,22 +21,26 @@ describe.only('Login', () => {
   // })
 
   // it('requires password', () => {
-  //   cy.get('[data-automation-id=phone]').type('Cypress.env('user').phone{enter}')
+  //   cy.get('[data-automation-id=phone]').type(`${Cypress.env('user').phone}{enter}`)
   //   cy.get('.error-msg')
   //     .should('contain', 'password number can\'t be blank')
   // })
 
-  // it('does not work with wrong credentials', () => {
-  //   cy.get('[data-automation-id=phone]').type('Cypress.env('user').phone{enter}')
-  //   cy.get('[data-automation-id=password]').type('invalid{enter}')
+  // it('does not work with wrong credentials', {
+  //   env: {
+  //     invalid_password: 'invalid'
+  //   }
+  // }, () => {
+  //   cy.get('[data-automation-id=phone]').type(`${Cypress.env('user').phone}{enter}`)
+  //   cy.get('[data-automation-id=password]').type("invalid{enter}")
   //   cy.get('.error-msg')
   //     .should('contain', 'phone or password is invalid')
   // })
 
-  it('displays home page on successful login', () => { 
+  it('displays home page on successful login', () => {
     // cy.get('[data-automation-id=phone]').type('Cypress.env('user').phone{enter}')
     // cy.get('[data-automation-id=password]').type('Cypress.env('user').password{enter}')
     cy.window().should('have.property', 'top')
-   })
+  })
 
 })
