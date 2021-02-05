61adf913b67506ba3dc8725afbc32112d21dd35b
Zhehlov Valentyn <vzhehlov@intersog.com>
2021-02-05 17:01:57 +0200

    fix unencoded password in database

--- backend/src/routes/settings/updateInfo.ts
+++ backend/src/routes/settings/updateInfo.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { configs } from '../../config';
-import { updateItem } from '../../dynamoAPI';
+import { getItem, updateItem } from '../../dynamoAPI';
 import { endpointRespond } from '../../utils';
+import { encrypt } from '../auth/utils';
 import { authenticateToken } from '../auth/validate';
 import { atLeast, isFailure } from '../types/guards';
 
@@ -17,12 +18,25 @@ updateInfo.post('/updateInfo', authenticateToken, async (req: any, res) => {
   if (!atLeast(password, xtoken, telegramId))
     return respond.FailureResponse('Required at least one field');
 
+  let encryptedPass = password;
+
+  if (password !== undefined) {
+    const userFromDB = await getItem(configs.USER_TABLE, {
+      username: req.user.data,
+    });
+
+    if (isFailure(userFromDB))
+      return respond.FailureResponse('Failed to update user info');
+
+    encryptedPass = encrypt(password, userFromDB.Item.key);
+  }
+
   const updateUserResponse = await updateItem(
     configs.USER_TABLE,
     {
       username: req.user.data,
     },
-    req.body
+    { ...req.body, password: encryptedPass }
   );
 
   if (isFailure(updateUserResponse))
