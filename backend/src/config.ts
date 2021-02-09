3ed92293732a8668ed1831fb32003b443eb7c27b
Leffffff <levonik99@gmail.com>
2021-02-09 10:56:46 +0200

    server set

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,12 +4,10 @@ export const secrets = {
   SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   REGION: process.env.AWS_REGION,
   SESSION_TOKEN: process.env.SESSION_TOKEN,
-  PASSPHRASE: process.env.PASSPHRASE,
 };
 
 export const configs = {
-  HTTPS_PORT: 443,
-  HTTP_PORT: 8000,
+  HTTP_PORT: 8080,
   USER_TABLE: 'users',
   MAX_AGE: 86400000,
   CURRENCY_CODE: {
