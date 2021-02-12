c15f452f16d6e9afedbed016a41a67cd909d7a83
Sergey Pylypas <nicknamegray@gmail.com>
2021-02-12 20:19:39 +0200

    Fixed refresh icon position

--- frontend/src/routes/Homepage.svelte
+++ frontend/src/routes/Homepage.svelte
@@ -116,7 +116,7 @@
           <img src="images/refresh.png" alt="refresh page" />
         </div>
         <div
-          class="telega h-8 w-8 flex cursor-pointer shadow-md rounded-2xl"
+          class="h-8 w-8 flex cursor-pointer shadow-md rounded-2xl ml-6"
           data-automation-id="telegram-link"
           on:click={() => window.open('https://t.me/exodus_MonobankBudgetBot')}
         >
