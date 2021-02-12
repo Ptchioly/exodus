f5290659f82b525164c13f2366c3959c6eed6d60
Elena Chornobai <echornobai@intersog.com>
2021-02-12 17:54:10 +0200

    CI: test 4

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -47,7 +47,7 @@ Cypress.Commands.add('registerUser', (options = {}) => {
   }
 
   const user = Cypress._.defaults({}, options, defaults)
-  cy.log(user)
+  cy.log(JSON.stringify(user))
   return cy.request({
     method: 'POST',
     url: `${Cypress.env("apiUrl")}/signup`,
