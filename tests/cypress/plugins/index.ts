56a35e0385906d3d7aea16d2183a24e0200970e9
Elena Chornobai <echornobai@intersog.com>
2021-02-15 12:52:26 +0200

    Cypress: improved configs for diffferent envs

--- tests/cypress/plugins/index.ts
+++ tests/cypress/plugins/index.ts
@@ -1,52 +1,70 @@
-// /// <reference types="cypress" />
+/// <reference types="cypress" />
+import fs from 'fs-extra'
+import path from 'path'
 
 // // import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 // // import { configs } from '../../../backend/src/config';
 
-// export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
-//   // on("task", {
 
-//   //   async "db:deleteUser"({ username, accessKeyId, secretAccessKey, region }) {
-//   //     const params = {
-//   //       TableName: configs.USER_TABLE,
-//   //       Key: { username },
-//   //     };
-//   //     const documentClient = new DocumentClient({
-//   //       accessKeyId: accessKeyId,
-//   //       secretAccessKey: secretAccessKey,
-//   //       region: region
-//   //     });
-//   //     return await documentClient
-//   //       .delete(params)
-//   //       .promise()
-//   //       .catch((err) => err);
-//   //   },
+export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+    const env = config.env.config || 'production'
+    switch (env) {
+        case 'development':
+            config.baseUrl = 'http://localhost:5000'
+            config.env.apiUrl = 'http://localhost:8080'
+            break;
+        case 'staging':
+            config.baseUrl = 'https://staging.beeeee.es'
+            config.env.apiUrl = 'https://staging-api.beeeee.es'
+            break;
+        case 'production':
+            break;
+    }
+    return config
 
-//   //   async "db:getUser"(
-//   //     { username,
-//   //       accessKeyId,
-//   //       secretAccessKey,
-//   //       region
-//   //     }
-//   //   ) {
-//   //     const params = {
-//   //       TableName: configs.USER_TABLE,
-//   //       Key: { username },
-//   //     };
+  // on("task", {
 
-//   //     const documentClient = new DocumentClient({
-//   //       accessKeyId: accessKeyId,
-//   //       secretAccessKey: secretAccessKey,
-//   //       region: region
-//   //     });
+  //   async "db:deleteUser"({ username, accessKeyId, secretAccessKey, region }) {
+  //     const params = {
+  //       TableName: configs.USER_TABLE,
+  //       Key: { username },
+  //     };
+  //     const documentClient = new DocumentClient({
+  //       accessKeyId: accessKeyId,
+  //       secretAccessKey: secretAccessKey,
+  //       region: region
+  //     });
+  //     return await documentClient
+  //       .delete(params)
+  //       .promise()
+  //       .catch((err) => err);
+  //   },
 
-//   //     return await documentClient
-//   //       .get(params)
-//   //       .promise()
-//   //       .catch((err) => err);
+  //   async "db:getUser"(
+  //     { username,
+  //       accessKeyId,
+  //       secretAccessKey,
+  //       region
+  //     }
+  //   ) {
+  //     const params = {
+  //       TableName: configs.USER_TABLE,
+  //       Key: { username },
+  //     };
 
-//   //   }
+  //     const documentClient = new DocumentClient({
+  //       accessKeyId: accessKeyId,
+  //       secretAccessKey: secretAccessKey,
+  //       region: region
+  //     });
 
-//   // });
-//   return config
-// }
+  //     return await documentClient
+  //       .get(params)
+  //       .promise()
+  //       .catch((err) => err);
+
+  //   }
+
+  // });
+  
+}
