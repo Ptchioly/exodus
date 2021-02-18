3f35b696a2ef3aca1cec6516fdf4917010765f9b
Elena Chornobai <echornobai@intersog.com>
2021-02-18 15:42:02 +0200

    Tests: removed commented out code

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -29,22 +29,6 @@ Cypress.Commands.add(
   }
 )
 
-// Cypress.Commands.add(
-//   "deleteMyUserIfExists",
-//   (user = { username: Cypress.env("username"), password: Cypress.env("password") }) => {
-//     cy.intercept(/login/).as("login")
-//     cy.loginByAPI(user)
-//     cy.wait("@login").then(interception =>
-//       interception.response?.statusCode === 200 ?
-//         cy.request({
-//           method: "DELETE",
-//           url: `${Cypress.env("apiUrl")}/deleteUser`,
-//         }) :
-//         cy.log(`Unable to login to delete user ${Cypress.env("username")}.`)
-//     )
-//   }
-// )
-
 Cypress.Commands.add(
   'deleteMyUserIfExists',
   (
@@ -140,8 +124,3 @@ Cypress.Commands.add('manualRegisterUser', (user = {}) => {
   cy.intercept('POST', 'signup').as('signup')
   cy.getBySel('signup-button').click()
 })
-
-// Cypress.Commands.add('setToken', (response) => {
-//   const token = response.headers['set-cookie'][0].match(/jwt=([^;]+)/)[1];
-//   return cy.setCookie('jwt', token, { expiry: configs.MAX_AGE })
-// })
