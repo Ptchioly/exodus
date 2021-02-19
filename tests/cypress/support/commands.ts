eedcb11afd8ba921ae0345d1455b6d6c16c450fa
Andrey Okun <aokun@intersog.com>
2021-02-19 18:55:30 +0200

    Removed tests on menu button visibility

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -84,7 +84,7 @@ Cypress.Commands.add('registerUserbyAPI', (options = {}) => {
 //  */
 Cypress.Commands.add('checkHomePageLoaded', () => {
   cy.getBySel('telegram-link', { timeout: 8000 }).should('be.visible')
-  cy.getBySel('menu-button').should('be.visible')
+  // cy.getBySel('menu-button').should('be.visible')
   cy.get('section>div.limits')
     .first()
     .should('exist') //not visible height 0px wtf??
