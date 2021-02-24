4988245033123e5f60254583fbf8250bbbc5c90f
Sergey Pylypas <nicknamegray@gmail.com>
2021-02-24 17:29:47 +0200

    fixed detailed view

--- frontend/src/charts/Bars.svelte
+++ frontend/src/charts/Bars.svelte
@@ -74,11 +74,12 @@
     bind:this={wrapper}
     class:moveable={false}
     on:mousedown={() => (isDetailed = !isDetailed)}
+    style='height: {isDetailed ? (bars.bars.length * 4) * (bars.conf.detailedSpace / 100) : 4}em'
 >
     <section class="bar-placeholder">
         <div
             class="bar--placeholder"
-            style="background: {bars.conf.background}"
+            style="background: {bars.conf.background}; height: {isDetailed ? (bars.bars.length * 2) * (bars.conf.detailedSpace / 100) + 2 : 2}em"
         />
     </section>
 
@@ -87,7 +88,7 @@
             <div
                 class="bar--wrapper"
                 class:detailed={isDetailed}
-                style="top: {isDetailed ? i * bars.conf.detailedSpace : 0}%"
+                style="top: {isDetailed ? i * bars.conf.detailedSpace : 0}%; z-index: {i*1};"
                 data-index={i + 1}
                 class:upperLayer={i === bars.bars.length - 1}
             >
@@ -115,7 +116,7 @@
                     </div>
                 </div>
                 <div class="limits" bind:this={limits}>
-                    {#each bars.limits as limit, limI}
+                    {#each bars.limits as limit, limitIndex}
                         {#if bar.limits && bar.limits.indexOf(limit.name) > -1}
                             <div
                                 class="limit"
@@ -124,7 +125,7 @@
                                 data-name={limit.name}
                                 style="{styledZIndex(
                                     limit.value < bar.value,
-                                    limI
+                                    limitIndex
                                 )}; left: {styledBarLeft(
                                     bar,
                                     limit
@@ -150,11 +151,8 @@
                                         >
                                             <div
                                                 class="remaining"
-                                                class:headless={limit.value -
-                                                    bar.value <
-                                                    100}
-                                                data-value="${limit.value -
-                                                    bar.value}"
+                                                class:headless={limit.value - bar.value < 100}
+                                                data-value="${limit.value - bar.value}"
                                                 style="border-color: {bar.background};"
                                             />
                                         </div>
@@ -188,6 +186,7 @@
         position: relative;
         width: 100%;
         user-select: none;
+        transition: height .3s;
     }
 
     .bar-placeholder {
@@ -202,6 +201,7 @@
         height: 2em;
         border-radius: 0.5em;
         background-color: #edf8f4;
+        transition: height .3s;
     }
 
     .bars {
@@ -300,8 +300,9 @@
     .limit-handle.moveable::after {
         content: attr(data-value);
         font-family: monospace;
+        font-size: .8em;
         font-weight: bold;
-        top: -1em;
+        top: -1.2em;
         position: absolute;
         z-index: 300;
         left: -0.1em;
