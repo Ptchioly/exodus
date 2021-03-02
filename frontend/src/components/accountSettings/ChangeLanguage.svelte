3f6d79a2a98060c1e551a8acdaf654f40cffa5ee
Maxim Tsurkan <mtsurkan@intersog.com>
2021-03-02 16:57:07 +0200

    resolve json modules in ts

--- frontend/src/components/accountSettings/ChangeLanguage.svelte
+++ frontend/src/components/accountSettings/ChangeLanguage.svelte
@@ -1,45 +1,48 @@
 <script lang="ts">
-    import { _, locale, dictionary } from "svelte-i18n";
-    import { onDestroy } from 'svelte'
-  
-    let languages: string[] = [];
-    let currentLanguage: string;
+  import { _, locale, dictionary } from 'svelte-i18n';
+  import { onDestroy } from 'svelte';
 
-    const cacheLanguage = (lang: string) => localStorage.setItem('language', lang);
-    const changeLanguage = (lang: string) => locale.set(lang);
+  let languages: string[] = [];
+  let currentLanguage: string;
 
-    const saveLanguage = (e: Event) => {
-        e.preventDefault();
-        changeLanguage(currentLanguage);
-        cacheLanguage(currentLanguage)
-    }
+  const cacheLanguage = (lang: string) =>
+    localStorage.setItem('language', lang);
+  const changeLanguage = (lang: string) => locale.set(lang);
 
-    const unsubscribeDict = dictionary.subscribe((dict) => {
-        languages.push(...Object.keys(dict));
-    })
-    const unsubscribeLoc = locale.subscribe((lang) => {
-        currentLanguage = lang;
-    })
+  const saveLanguage = (e: Event) => {
+    changeLanguage(currentLanguage);
+    cacheLanguage(currentLanguage);
+  };
 
-    onDestroy(() => {
-        unsubscribeDict();
-        unsubscribeLoc();
-    })
+  const unsubscribeDict = dictionary.subscribe((dict) => {
+    languages.push(...Object.keys(dict));
+  });
+  const unsubscribeLoc = locale.subscribe((lang) => {
+    currentLanguage = lang;
+  });
+
+  onDestroy(() => {
+    unsubscribeDict();
+    unsubscribeLoc();
+  });
 </script>
 
 <div class="flex flex-col w-400">
-    <div class="flex flex-row mt-2 items-center">
-        <div>{$_("settings.change_lang.languages")}</div>
-        <select class='ml-5 border-gray-400 border-2 rounded-md pl-3 mr-5' bind:value={currentLanguage}>
-            {#each languages as language}
-                <option value={language}>{language}</option>
-            {/each}
-        </select>
-    </div>
-    <button
-        class="mt-4 self-start bg-coolGreen-default py-1 px-3 rounded-md text-white"
-        data-automation-id="change-xtoken"
-        on:click={saveLanguage}>{$_("settings.change_lang.btn")}</button
+  <div class="flex flex-row mt-2 items-center">
+    <div>{$_('settings.change_lang.languages')}</div>
+    <select
+      class="ml-5 border-gray-400 border-2 rounded-md pl-3 mr-5"
+      bind:value={currentLanguage}
     >
+      {#each languages as language}
+        <option value={language}>{language}</option>
+      {/each}
+    </select>
+  </div>
+  <button
+    class="mt-4 self-start bg-coolGreen-default py-1 px-3 rounded-md text-white"
+    data-automation-id="change-xtoken"
+    on:click|preventDefault={saveLanguage}
+    >{$_('settings.change_lang.btn')}</button
+  >
 </div>
-  
